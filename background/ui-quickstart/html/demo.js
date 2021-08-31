2eaa02debfd54f6d72c02e93cb8b4c7ab119d6a7
Sam Bhattacharyya <sam@vectorly.io>
2021-08-31 16:40:14 -0700

    Simplified it even more

--- background/ui-quickstart/html/demo.js
+++ background/ui-quickstart/html/demo.js
@@ -1,81 +1,74 @@
 /* eslint-disable */
 
-console.log('Feature Check', vectorly.BackgroundFilter.isSupported());
+const BackgroundFilter = vectorly.BackgroundFilter;
+
+
+async function startStream() {
+
+  const processed = document.getElementById('demo');
+
+  const inputStream = await navigator.mediaDevices.getUserMedia({audio: false, video: true})
+
+  if (!BackgroundFilter.isSupported()) {
+
+    alert("Virtual Backgrounds is not supported on this browser");
+    processed.srcObject = inputStream;
+
+  } else {
+
+    try {
+      const outputStream = await enablebackground(inputStream);
+      processed.srcObject = outputStream;
+
+    } catch (e) {
+      alert("Unable to start virtual backgrounds", e);
+      processed.srcObject = inputStream;
+    }
+
+
+  }
+}
+
+  async function enablebackground(inputStream) {
+
+    const params = {
+      token: getUrlParams('token') || '0b5707c6-6642-4cc8-8570-b29af9e51345',
+      passthrough: true
+    }
+    const bgFilter = new BackgroundFilter(inputStream, params);
+    window.bgFilter = bgFilter
+    return await bgFilter.getOutput()
+
+
+  }
+
+  async function disablebackground() {
+    if (window.bgFilter) window.bgFilter.disable()
+  }
+
+
+  function updatevirtualbackground(image) {
+    if (window.bgFilter) {
+      window.bgFilter.enable()
+      window.bgFilter.changeBackground(image);
+    }
+  }
+
+
+
+  window.addEventListener('load', async function () {
+
+    try {
+      startStream();
+    } catch (e) {
+      alert("Unable to initialize webcam", e);
+    }
+
+  });
+
+
 
 function getUrlParams(prop) {
   window.searchParams = window.searchParams || (new URLSearchParams(window.location.search));
   return window.searchParams.get(prop)
 }
-
-
-async function initializeWebcam(){
-
-  window.mediaStream = await navigator.mediaDevices.getUserMedia(window.constraints || {audio: false, video: true})
-
-}
-
-
-async function streamWebcam() {
-  try{
-    if (!('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia)) {
-      throw "webcam initialization failed"
-    }
-
-    let loadingGif = document.getElementById('loading');
-    loadingGif.style.display = "block";
-
-    await initializeWebcam();
-    await enablebackground()
-
-    loadingGif.style.display = "none";
-  } catch (e) {
-    alert("webcam initialization failed")
-  }
-}
-
-async function stopWebcam(){
-  window.mediaStream.getTracks()[0].stop()
-  window.bgFilter && window.bgFilter.stop()
-}
-
-async function enablebackground() {
-
-  const processed = document.getElementById('demo');
-
-  const params = {
-    token: getUrlParams('token') || '0b5707c6-6642-4cc8-8570-b29af9e51345',
-    passthrough: true
-  }
-
-  try {
-    const bgFilter = new vectorly.BackgroundFilter(window.mediaStream, params);
-    window.bgFilter = bgFilter
-    const outputStream = await bgFilter.getOutput()
-    processed.srcObject = outputStream
-  } catch (err) {
-    processed.srcObject = window.mediaStream;
-
-  }
-
-}
-
-async function disablebackground(){
-
-  if (window.bgFilter) {
-    window.bgFilter.disable()
-
-  }
-}
-
-
-function updatevirtualbackground(image){
-  if (window.bgFilter) {
-    window.bgFilter.enable()
-    window.bgFilter.changeBackground(image);
-  }
-}
-
-
-window.onload = (event) => {
-  streamWebcam();
-};
